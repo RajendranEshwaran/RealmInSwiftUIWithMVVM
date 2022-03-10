a4feee8f5e29c5e4d237a6227fb9f475334423f9
RajendranEshwaran <rajendranmca2010@gmail.com>
2022-03-10 10:10:09 -0500

    Initial Commit

---
+++ RealmWithSwiftUI/RealmWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmWithSwiftUIApp.swift
+//  RealmWithSwiftUI
+//
+//  Created by gomathi saminathan on 3/10/22.
+//
+
+import SwiftUI
+
+@main
+struct RealmWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
